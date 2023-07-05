272bfe363547e23a5901756e85aab638adea6e90
AyoubMS <chahayb563@gmail.com>
2023-07-05 14:28:24 +0200

    add app

---
+++ PropertyTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+
+using PropertyTypes;
+
+AddressModel address = new AddressModel();
+address.StreetAddress = "ABC";
+address.City = "Casa";
+address.State = "Casablanca";
+address.ZipCode = "20120";
+
+Console.WriteLine(address.FullAddress);
